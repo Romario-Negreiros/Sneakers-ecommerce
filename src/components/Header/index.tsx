69a81d8019f93479f93bde0fff5bf25cb7d0a2a2
Romario Negreiros <nromario482@gmail.com>
2023-06-04 20:53:38 -0300

    Add mobile and desktop html structure and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,53 +1,91 @@
+'use client'
+
+import { useRef } from 'react'
+
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function Header() {
+import styles from './styles.module.css'
+
+import type { MouseEvent } from 'react'
+
+type MenuActions = 'add' | 'remove'
+
+const Header = () => {
+  const navRef = useRef<HTMLElement>(null)
+
+  const handleMenu = (
+    ev: MouseEvent<HTMLButtonElement>,
+    action: MenuActions
+  ) => {
+    ev.preventDefault()
+    if (navRef.current !== null) {
+      navRef.current.classList[action](styles.navigationActive)
+    }
+  }
+
   return (
-    <header>
+    <header className={styles.container}>
       <div>
-        <div>
-          <button>
-            <Image alt="Open menu" src="/icon-menu.svg" fill />
-          </button>
+        <div className={styles.logoMenuContainer}>
+          <div className={styles.openBtn}>
+            <button
+              onClick={ev => {
+                handleMenu(ev, 'add')
+              }}
+            >
+              <Image alt="Open menu" src="/icons/icon-menu.svg" fill />
+            </button>
+          </div>
+          <div>
+            <Image alt="Sneaking logo" src="/logo.svg" fill />
+          </div>
         </div>
-        <div>
-          <Image alt="Sneaking logo" src="/logo.svg" fill />
-        </div>
-      </div>
-      <nav>
-        <div>
-          <button>
-            <Image alt="Close menu" src="/icon-close.svg" fill />
-          </button>
-        </div>
-        <ul>
-          <li>
-            <Link href="">Collections</Link>
-          </li>
-          <li>
-            <Link href="">Men</Link>
-          </li>
-          <li>
-            <Link href="">Women</Link>
-          </li>
-          <li>
-            <Link href="">About</Link>
-          </li>
-          <li>
-            <Link href="">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <div>
-          <button>
-            <Image alt="Go to cart" src="/icon-cart.svg" fill />
-          </button>
-        </div>
-        <div>
-          <Image alt="Go to your profile" src="/image-avatar.png" fill />
+        <nav className={styles.navigation} ref={navRef}>
+          <div>
+            <div className={styles.closeBtn}>
+              <button
+                onClick={ev => {
+                  handleMenu(ev, 'remove')
+                }}
+              >
+                <Image alt="Close menu" src="/icons/icon-close.svg" fill />
+              </button>
+            </div>
+            <ul>
+              <li>
+                <Link href="">Collections</Link>
+              </li>
+              <li>
+                <Link href="">Men</Link>
+              </li>
+              <li>
+                <Link href="">Women</Link>
+              </li>
+              <li>
+                <Link href="">About</Link>
+              </li>
+              <li>
+                <Link href="">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className={styles.actionsContainer}>
+          <div>
+            <button>
+              <Image alt="Go to cart" src="/icons/icon-cart.svg" fill />
+            </button>
+          </div>
+          <div>
+            <Link href="">
+              <Image alt="Go to your profile" src="/image-avatar.png" fill />
+            </Link>
+          </div>
         </div>
       </div>
     </header>
   )
 }
+
+export default Header
