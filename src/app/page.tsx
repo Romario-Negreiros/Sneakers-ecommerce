6330c4b44ee9ec920f1f21843be9c1ad819714b6
Romario Negreiros <nromario482@gmail.com>
2023-06-13 20:59:09 -0300

    Add banner html structure and add Slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { Cart, Slider } from '../components'
+import Link from 'next/link'
+
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>a</main>
+    <main className={styles.container}>
+      <section className={styles.fullHeight}>
+        <div className={styles.banner}>
+          <div className={styles.sliderContainer}>
+            <Slider />
+          </div>
+          <div className={styles.bannerTexts}>
+            <p className={styles.itemText}>
+              For sale now - <span>30% OFF!</span>
+            </p>
+            <Link href="">
+              <Cart />
+              <span>Add to cart</span>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
